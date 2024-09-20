016977ecfc3b43b080d74e76c8ff19699448e810
Anastasia-Erileeva-kt-42-21 <nastyaeril@gmail.com>
2024-09-20 12:23:59 +0300

    add logging and WeatherForecast test request

--- AnastasiaErileevaKt-42-21/obj/Debug/net7.0/AnastasiaErileevaKt-42-21.MvcApplicationPartsAssemblyInfo.cs
+++ AnastasiaErileevaKt-42-21/obj/Debug/net7.0/AnastasiaErileevaKt-42-21.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("AnastasiaErileevaKt-42-21")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a9b7c33f402b7feafb812ea898475056220b42")]
-[assembly: System.Reflection.AssemblyProductAttribute("AnastasiaErileevaKt-42-21")]
-[assembly: System.Reflection.AssemblyTitleAttribute("AnastasiaErileevaKt-42-21")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.OpenApi")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
